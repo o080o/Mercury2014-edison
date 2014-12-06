c4d3d68a591e8bba26ccfe240024a2d94105f90e
Alex <o0o.moro.o0o@gmail.com>
2014-12-06 10:39:54 +0000

    added support to detect power button presses

--- led_control.lua
+++ led_control.lua
@@ -10,7 +10,7 @@ int usleep(int);
 
 
 
-local led = led( gpio.pinmap[17][1], gpio.pinmap[18][1], gpio.pinmap[18][7] ,10000)
+local led = led( gpio.pinmap[18][7], gpio.pinmap[18][1], gpio.pinmap[17][1] ,10000)
 
 
 local PERIOD = 100
@@ -18,9 +18,9 @@ while true do
 	local f = 0
 	while f<PERIOD do
 		local r,g,b = color.HSVtoRGB( f/PERIOD, 1, 1 )
-		led:color( r,g,b )
+		led:color( (1-r),(1-g*.1),(1-b*.2) )
 		f = f + 1
-		ffi.C.usleep(20000)
+		ffi.C.usleep(40000)
 	end
 end
 
