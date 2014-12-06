de041295270d10b38210da150dafe18c5fc767df
Alex <o0o.moro.o0o@gmail.com>
2014-12-06 09:54:18 +0000

    added luajit's ffi to use usleep function for timing

--- led_control.lua
+++ led_control.lua
@@ -2,16 +2,25 @@ local gpio = require("gpio")
 local led = require("led")
 local color = require("color")
 
+local ffi = require("ffi")
+ffi.cdef[[
+int sleep(int);
+int usleep(int);
+]]
+
+
+
 local led = led( gpio.pinmap[17][1], gpio.pinmap[18][1], gpio.pinmap[18][7] ,10000)
 
 
-local PERIOD = 8000
+local PERIOD = 100
 while true do
 	local f = 0
 	while f<PERIOD do
 		local r,g,b = color.HSVtoRGB( f/PERIOD, 1, 1 )
 		led:color( r,g,b )
 		f = f + 1
+		ffi.C.usleep(20000)
 	end
 end
 
--- lights.sh
+++ lights.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Script used to start a lua script via systemd
 cd /home/root/Mercury2014-edison/
-lua led_control.lua
+luajit led_control.lua
