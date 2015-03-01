dab3ec14cc85a9c39675e7235e7f74d809644cde
Alex <o0o.moro.o0o@gmail.com>
2015-03-01 00:24:56 +0000

    backing up to github servers...

--- leddaemon.lua
+++ leddaemon.lua
@@ -11,13 +11,23 @@ int usleep(int);
 ]]
 
 local function wifiMode()
-	local apd = io.popen( "systemctl | grep hostapd" )
-	local isAPMode = string.len( apd:read( "*all" ) ) > 0 
-	local ifconfig = io.popen( "ifconfig | grep wlan0 -A 3")
-	local wificonfig = ifconfig:read("*all"):match("UP BROADCAST RUNNING MULTICAST")
-	if not wificonfig then return "offline" end
-	if isAPMode then return "ap"
-	else return "wifi" end
+	local apd, err1 = io.popen( "systemctl | grep hostapd" )
+	local ifconfig, err2 = io.popen( "ifconfig | grep wlan0 -A 3")
+	if apd and ifconfig then
+		local isAPMode = string.len( apd:read( "*all" ) ) > 0 
+		local wificonfig = ifconfig:read("*all"):match("UP BROADCAST RUNNING MULTICAST")
+		apd:close(); ifconfig:close()
+		if not wificonfig then return "offline" end
+		if isAPMode then return "ap"
+		else return "wifi" end
+	else
+		if apd then apd:close() end
+		if ifconfig then ifconfig:close() end
+		print(err1)
+		print(err2)
+		return "err"
+	end
+
 end
 
 
@@ -28,6 +38,7 @@ local state = "idle"
 
 local led = led( gpio.pinmap[18][7], gpio.pinmap[18][1], gpio.pinmap[17][1] ,10000)
 local btn = PwrBtn()
+
 function btn.down()
 	state = "btn"
 end
@@ -35,6 +46,7 @@ function btn.up()
 	state = "idle"
 	led:color( 1, 0, 0 )
 	local mode = wifiMode()
+	-- topple between wifi connection and Acess Point mode
 	if mode=="ap" then os.execute( "configure_edison --disableOneTimeSetup" )
 	else os.execute( "configure_edison --enableOneTimeSetup" ) end
 end
@@ -55,6 +67,14 @@ function states.wifi(f)
 	local r,g,b = color.HSVtoRGB( (f%PERIOD)/PERIOD, 1, 1)
 	led:color( (1-r),(1-g*.1),(1-b*.2) ) 
 end
+function states.err(f)
+	if f%2 == 0 then
+	led:color( 1,0,0)
+	else
+	led:color( 1,1,1)
+	end
+end
+
 local FLASHPERIOD = 2
 function states.offline(f)
 	--local r = math.floor(( 2*(f % FLASHPERIOD))/FLASHPERIOD)
